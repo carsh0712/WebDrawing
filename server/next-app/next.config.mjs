aa189bc1d288e72d5159c7dff900fe150bb23edc
carsh0712 <carsh0712@gmail.com>
2026-08-20 17:44:36 +0900

    routing 버그 수정

--- server/next-app/next.config.mjs
+++ server/next-app/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
+  async rewrites() {
+    return [
+      {
+        source: '/gallery',
+        destination: '/index.html',
+      },
+      {
+        source: '/canvas',
+        destination: '/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
